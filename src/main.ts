9da052ccf265b850e67ff7168867f7b4fd518253
khaelar <midavmidav2000@gmail.com>
2024-02-25 16:00:18 +0200

    feat: update thumbnail on resize; minor improvements

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { around } from "monkey-around";
-import { Plugin, Canvas, CanvasView, LinkNodeConstructor } from "obsidian";
+import { Plugin, Canvas, CanvasView, LinkNodeConstructor, debounce } from "obsidian";
 import { NativeImage } from "electron";
-import { createHash } from "crypto";
 
 export default class CanvasLinkOptimizerPlugin extends Plugin {
 	name = "Canvas Link Optimizer";
@@ -58,91 +57,129 @@ export default class CanvasLinkOptimizerPlugin extends Plugin {
 	patchLinkNode(constructor: LinkNodeConstructor): boolean {
 		const thisPlugin = this;
 		const uninstaller = around(constructor.prototype, {
-			initialize: (next: any) =>
-				function (...args: any[]) {
-					this._initializing = true;
-					const result = next.call(this, ...args);
-					this._initializing = false;
+			_saveThumbnail: () => async function () {
+				if (!this.frameEl) return;
 
+				thisPlugin.log(`Saving thumbnail for ${this.url}`)
+				const img: NativeImage = await this.frameEl.capturePage();
+				this.app.vault.adapter.writeBinary(this._getThumbnailPath(),
+					img.toJPEG(100));
+			},
+			_getThumbnailPath: () => function () {
+				return `${thisPlugin.cacheDir}/${this.id}.thumbnail.jpg`
+			},
+			_getMetadataPath: () => function () {
+				return `${thisPlugin.cacheDir}/${this.id}.metadata.json`
+			},
+			initialize: (next: any) => function (...args: any[]) {
+				this._initializing = true;
+				const result = next.call(this, ...args);
+				this._initializing = false;
+
+				// adding resize handler
+				const saveThumbnail = debounce(() => this._saveThumbnail(), 500);
+				const resizeObserver = new MutationObserver(
+					async (mutationList) => {
+						for (const mutation of mutationList) {
+							if (
+								mutation.type !== "attributes" ||
+								mutation.attributeName !== "style"
+							) continue;
+
+							const target = mutation.target as HTMLElement;
+							const newWidth = target.style.width;
+							const newHeight = target.style.height;
+
+							const oldValue = mutation.oldValue || "";
+							const oldWidthMatch = oldValue.match(/width:\s*([^;]+)(;|$)/);
+							const oldHeightMatch = oldValue.match(/height:\s*([^;]+)(;|$)/);
+							const oldWidth = oldWidthMatch ? oldWidthMatch[1].trim() : "";
+							const oldHeight = oldHeightMatch ? oldHeightMatch[1].trim() : "";
+
+							if (newWidth !== oldWidth || newHeight !== oldHeight) {
+								saveThumbnail();
+							}
+						}
+					}
+				);
+
+				const config = {
+					attributes: true,
+					attributeOldValue: true,
+					attributeFilter: ["style"]
+				};
+				resizeObserver.observe(this.nodeEl, config);
+
+				// displaying thumbnail
+				(async () => {
 					const revealWebview = () => {
 						this.alwaysKeepLoaded = true;
 						this._perviewImageEl?.remove?.();
 						this.recreateFrame();
 					}
 
-					const thumbnailPath = thisPlugin.getLinkCachePath(this.url, "thumbnail.jpg");
-					const metadataPath = thisPlugin.getLinkCachePath(this.url, "metadata.json");
-
-					(async () => {
-						const [thumbnailExists, metadataExists] = await Promise.all([
-							thisPlugin.app.vault.exists(thumbnailPath),
-							thisPlugin.app.vault.exists(metadataPath)
-						]);
-						if (!thumbnailExists || !metadataExists) {
-							// url isn't cached, loading webview
-							this.recreateFrame();
-							return;
-						}
-
-						this.alwaysKeepLoaded = false;
-
-						// setting cached title
-						const metadataRaw = await thisPlugin.app.vault.adapter.read(metadataPath);
-						const metadata = JSON.parse(metadataRaw);
-						this.updateNodeLabel(metadata.title);
-
-						this._perviewImageEl = this.contentEl.doc.createElement("img");
-						this.contentEl.append(this._perviewImageEl);
-						this._perviewImageEl.classList.add("link-thumbnail");
-						this._perviewImageEl.alt = "Webpage thumbnail";
-
-						// displaying cached thumbnail
-						this._perviewImageEl.src = thisPlugin.app.vault.adapter
-							.getResourcePath(thumbnailPath);
-
-						this._perviewImageEl.addEventListener("click", () => {
-							this.alwaysKeepLoaded = true;
-							this._perviewImageEl?.remove?.();
-							this.recreateFrame();
-						});
-					})();
-
-					return result;
-				},
-
-			recreateFrame: (next: any) =>
-				function (...args: any[]) {
-					if (this._initializing) return null;
-
-					const result = next.call(this, ...args);
-
-					// wont trigger on mobile
-					if (this.frameEl?.tagName === "WEBVIEW") {
-						const onFrameLoaded = async () => {
-							this.frameEl.removeEventListener("did-frame-finish-load", onFrameLoaded);
-
-							// some pages aren't fully initialized at this moment, so waiting a bit more
-							await sleep(1000);
-
-							// saving page title
-							const metadataPath = thisPlugin.getLinkCachePath(this.url, "metadata.json");
-							this.app.vault.adapter.write(metadataPath, JSON.stringify({
-								title: this.frameEl.getTitle()
-							}));
-
-							// saving captured thumbnail
-							const img: NativeImage = await this.frameEl.capturePage();
-							const thumbnailPath = thisPlugin.getLinkCachePath(this.url, "thumbnail.jpg");
-							this.app.vault.adapter.writeBinary(thumbnailPath, img.toJPEG(100));
-
-							thisPlugin.log(`Cached link ${this.url}`);
-						}
-
-						this.frameEl.addEventListener("did-frame-finish-load", onFrameLoaded);
+					const [thumbnailExists, metadataExists] = await Promise.all([
+						thisPlugin.app.vault.exists(this._getThumbnailPath()),
+						thisPlugin.app.vault.exists(this._getMetadataPath())
+					]);
+					if (!thumbnailExists || !metadataExists) {
+						// url isn't cached, loading webview
+						this.recreateFrame();
+						return;
 					}
 
-					return result;
+					this.alwaysKeepLoaded = false;
+
+					// setting cached title
+					const metadataRaw = await thisPlugin.app.vault.adapter.read(
+						this._getMetadataPath());
+					const metadata = JSON.parse(metadataRaw);
+					this.updateNodeLabel(metadata.title);
+
+					this._perviewImageEl = this.contentEl.doc.createElement("img");
+					this.contentEl.append(this._perviewImageEl);
+					this._perviewImageEl.classList.add("link-thumbnail");
+					this._perviewImageEl.alt = "Webpage thumbnail";
+
+					// displaying cached thumbnail
+					this._perviewImageEl.src = thisPlugin.app.vault.adapter
+						.getResourcePath(this._getThumbnailPath());
+
+					this._perviewImageEl.addEventListener("click", revealWebview);
+					this._perviewImageEl.addEventListener("error", revealWebview);
+				})();
+
+				return result;
+			},
+
+			recreateFrame: (next: any) => function (...args: any[]) {
+				if (this._initializing) return null;
+
+				const result = next.call(this, ...args);
+
+				// wont trigger on mobile
+				if (this.frameEl?.tagName === "WEBVIEW") {
+					const onFrameLoaded = async () => {
+						this.frameEl.removeEventListener("did-frame-finish-load", onFrameLoaded);
+
+						// some pages aren't fully initialized at this moment, so waiting a bit more
+						await sleep(1000);
+
+						// saving page title
+						const metadataPath = this._getMetadataPath();
+						this.app.vault.adapter.write(metadataPath, JSON.stringify({
+							title: this.frameEl.getTitle()
+						}));
+
+						await this._saveThumbnail();
+						thisPlugin.log(`Cached link ${this.url}`);
+					}
+
+					this.frameEl.addEventListener("did-frame-finish-load", onFrameLoaded);
 				}
+
+				return result;
+			}
 		});
 		this.register(uninstaller);
 
@@ -166,10 +203,5 @@ export default class CanvasLinkOptimizerPlugin extends Plugin {
 		const dummyLinkNode = canvasInstance.createLinkNode.call(
 			dummyCanvasInstance, dummyNodeParams);
 		return dummyLinkNode.constructor;
-	}
-
-	getLinkCachePath(url: string, suffix: string): string {
-		const linkHash = createHash('sha256').update(url).digest("hex");
-		return `${this.cacheDir}/${linkHash.slice(0, 16)}.${suffix}`;
 	}
 }
